a78906b2b2bfaec1981222ba58a9c9f8ca502d53
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2023-10-19 21:43:57 +0200

    Effects applied

--- lab2/anacotokomu/main.js
+++ lab2/anacotokomu/main.js
@@ -1,34 +1,39 @@
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1; //declare variable which follows current slide
+showSlides(slideIndex); //show slide with current slide
 
-// Ustaw interwał czasowy, aby automatycznie zmieniać slajdy co 3 sekundy
+// set time interval for 3s
 setInterval(() => {
-  plusSlides(1); // Przesuwa się do następnego slajdu
-}, 3000); // 3000 milisekundy = 3 sekundy
+  plusSlides(1); // go to next slide +1
+}, 3000); 
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+// Go to selected slide(dots)
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+//Showing the right slide
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex = 1} //for example for 4. slide go to the first one in this case
+  if (n < 1) {slideIndex = slides.length} //if you click prev on first slide go to the last one
+  //hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  //removing 'active' class which is used to indicate which slide is currently chosen
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+  //make chosen slide visible
   slides[slideIndex-1].style.display = "block";
+  //make dot for chosen slide visible
   dots[slideIndex-1].className += " active";
 }
