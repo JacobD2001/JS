a69ecebe96ca21174634c460d46c3f5d7dfea1cb
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2023-11-21 21:01:59 +0100

    task completed(can be optimized)

--- lab5/Async operations/main.js
+++ lab5/Async operations/main.js
@@ -1,20 +1,41 @@
+//can be optimized
 
-const addButton = document.getElementById('addButton');
-
-    async function asyncAdd(a, b) {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve(a + b);
-            }, 100);
-        });
-    }
-    
-    addButton.addEventListener('click', async () => {
-        const number1 = parseInt(document.getElementById('number1').value, 10) || 0;
-        const number2 = parseInt(document.getElementById('number2').value, 10) || 0;
-
-        const result = await asyncAdd(number1, number2);
-
-        document.getElementById('result').value = result;
+async function asyncAdd(...numbers) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const sum = numbers.reduce((acc, num) => acc + num, 0);
+        resolve(sum);
+      }, 100);
     });
-
+  }
+  
+  function measureExecutionTime(func, ...args) {
+    const start = performance.now();
+    const result = func(...args);
+    const end = performance.now();
+    const executionTime = end - start;
+  
+    return { result, executionTime };
+  }
+  
+  async function performAsyncAddition(numbersArray) {
+    let asyncOperations = 0;
+  
+    const resolvedNumbers = await Promise.all(numbersArray);
+    const result = await asyncAdd(...resolvedNumbers.map(num => {
+      asyncOperations++;
+      return num;
+    }));
+  
+    // Update HTML elements with results
+    document.getElementById('result').textContent = `Result: ${result}`;
+    document.getElementById('executionTime').textContent = `Execution time: ${executionTime} ms`;
+    document.getElementById('asyncOperations').textContent = `Number of async operations: ${asyncOperations}`;
+  }
+  
+  // Create an array with 100 promises
+  const numbersArray = Array.from({ length: 100 }, (_, index) => Promise.resolve(index + 1));
+  
+  // Trigger the asynchronous addition and measure execution time
+  const { result, executionTime, asyncOperations } = measureExecutionTime(performAsyncAddition, numbersArray);
+  
