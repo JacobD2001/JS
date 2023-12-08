2ec91b692091e506510fb489b8b1a7595b58a508
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2023-12-08 20:25:26 +0100

    sledzik 1 completed

--- lab8/Sledzik/index.js
+++ lab8/Sledzik/index.js
@@ -1,34 +1,99 @@
-document.getElementById("button").addEventListener("click", GetWeather);
-//document.getElementById("inputCity").addEventListener
+document.getElementById('getWeatherBtn').addEventListener('click', () => getWeather(document.getElementById('inputCity').value));
+document.getElementById('savePlaceBtn').addEventListener('click', savePlace);
+//function to get weather based on city name
+function getWeather() {
+    const apiKey = '7776b7cf6d82e39b67011a753fbc94a9';
+    const city = document.getElementById('inputCity').value;
+    console.log('getting weather for: ', city)
+    if (city === '') {
+        alert('Please give the city name');
+        console.log('no city name given');
+        return;
+    }
 
-function GetWeather() {
-const apiKey = 'https://api.openweathermap.org/data/3.0/onecall?appid={a20aae86e9485e5be4ddad135579ffff}';
-const city = document.getElementById("inputCity").value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-if(city === '') {
-    alert('Please enter the city');
-    return;
-}
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const temperature = data.main.temp;
+            const feelsLike = data.main.feels_like;
+            const minTemperature = data.main.temp_min;
+            const maxTemperature = data.main.temp_max;
+            const pressure = data.main.pressure;
+            const humidity = data.main.humidity;
+            const weatherDescription = data.weather[0].description;
+            const weatherIcon = data.weather[0].icon; // Get the icon code
 
-fetch(apiUrl)
-.then(Response => Response.json())
-.then(data => {
-    // Przetwórz dane o pogodzie
-    const temperature = data.main.temp;
-    const humidity = data.main.humidity;
-    const weatherDescription = data.weather[0].description;
 
-    // Wyświetl informacje o pogodzie
-    const weatherInfo = document.getElementById('weatherInfo');
-    weatherInfo.innerHTML = `
-        <p>Temperature: ${temperature} K</p>
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `
+        <p>Temperature: ${temperature} C</p>
+        <p>Feels Like: ${feelsLike} C</p>
+        <p>Min Temperature: ${minTemperature} C</p>
+        <p>Max Temperature: ${maxTemperature} C</p>
+        <p>Pressure: ${pressure} hPa</p>
         <p>Humidity: ${humidity}%</p>
         <p>Description: ${weatherDescription}</p>
+        <img src="http://openweathermap.org/img/w/${weatherIcon}.png" alt="weather icon">
     `;
-})
-.catch(error => {
-    console.error('Błąd pobierania danych o pogodzie:', error);
-});
+        })
+        .catch(error => {
+            console.error('error fetching data', error);
+        });
+}
 
+//function to save places in local storage
+function savePlace() {
+    const city = document.getElementById('inputCity').value;
+    if (city === '') {
+        alert('Please give the city name');
+        console.log('no city name given');
+        return;
+    }
 
+    const places = getPlaces();
+    if (places.length >= 10) {
+        alert('You can only save up to 10 cities.');
+        return;
+    }
+
+    places.push(city);
+    localStorage.setItem('places', JSON.stringify(places));
+    console.log('place saved', city);
+}
+
+//function to get places from local storage
+function getPlaces() {
+    const storedPlaces = localStorage.getItem('places');
+    if (storedPlaces) {
+        const parsedPlaces = JSON.parse(storedPlaces);
+        if (Array.isArray(parsedPlaces)) {
+            return parsedPlaces;
+        }
+    }
+    return [];
+}
+
+// Get the weather for all stored cities when the app starts(FOR Now just get the places)
+window.onload = () => {
+    const places = getPlaces();
+    displayPlaces(places);
+   // places.forEach(city => getWeather(city));
+    console.log('places loaded', places);
+};
+
+ // Function to display places in the HTML
+ function displayPlaces(places) {
+    const placesList = document.getElementById('placesList');
+
+    // Clear existing content in the placesList div
+    placesList.innerHTML = '';
+
+    // Create and append HTML elements for each place
+    places.forEach(city => {
+        const placeElement = document.createElement('div');
+        placeElement.textContent = city;
+        placesList.appendChild(placeElement);
+    });
 }
