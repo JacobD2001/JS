5d469983c12deb5c6e6ad7e2c205cd8c3549ba29
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2023-12-08 13:18:33 +0100

    observable added(but 2 methods)

--- lab7/ToDo 3/index.js
+++ lab7/ToDo 3/index.js
@@ -1,7 +1,6 @@
 //TO DO3: third-party library
 
 interval(saveCToSessionStorage) //passes saveCToSessionStorage to interval
-
 //sets up a timer that increments every 2 seconds(each time calling 2 functions)
 //setInterval says execute interval every 2 seconds
 //inside setInterval there is an anonymous function(without a name) that calls callback function
@@ -16,36 +15,54 @@ function interval(callback) {
     , 2000)
 }
 
+class Observable {
+  constructor() {
+    this.observers = []; // observers: an array of observers that will get notified whenever a specific event occurs
+  }
+
+  addObserver(obs) {
+    this.observers.push(obs);
+  }
+  removeObserver(obs) {
+    this.observers = this.observers.filter((observer) => observer !== obs);
+  }
+  notify(data) {
+    this.observers.forEach((observer) => observer(data));
+  }
+}
 
 //logs provided data to console
 //third party lib 
 class Logger {
   static log(data) {
-    console.log(data);
+    console.log(`[log from C] ${data}`);
+    //console.log(`[powerball number]  ${data.number}`);
+
   }
 
-  static logFromC(data) {
-    console.log('[reader C]', data)
-    Logger.log(`[log from C] ${data}`);
+  static log1(number){
+    console.log(`[powerball number]  ${number}`)
   }
 
-  static logPowerBallNumber(number){
-    console.log('[powerball number]', number)
-  }
-  
 }
 
+const logger = new Logger();
+const observable = new Observable();
+const observable1 = new Observable();
+
+observable.addObserver(Logger.log);
+observable1.addObserver(Logger.log1);
 
 function saveCToSessionStorage(data) { //takes data
-  Logger.logFromC(data); //logs data
   const storageData = { data } //store data
   sessionStorage.setItem('C', JSON.stringify(storageData)) //store data in session storage
+  observable.notify(data);
 }
 
 //generates a random number based on data & logs it
 function discoverPowerBallNumber(data) {
   const number = Math.floor(Math.random() * data * 100)
-  Logger.logPowerBallNumber(number)
+  observable1.notify(number);
 }
 
 
