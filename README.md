6558b108b913f504e1f20f5e87ecfb0520e61d61
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2023-12-22 01:40:58 +0100

    NicSieNieDzialo Completed

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
  - lab 6: Do evrth
  - lab 7: ✔️
  - lab 8: last christmas - chart
- - lab 9: 1(STARt reset measure), 2,3 TODO
+ - lab 9: 2,3 TODO
 
  ##Pytania:
  1. Last christmas - how to do chart?
--- lab9/NicSieNieDzialo/index.js
+++ lab9/NicSieNieDzialo/index.js
@@ -1,6 +1,31 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d'); // how does getcontext work? - https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext - it returns a drawing context on the canvas, or null if the context identifier is not supported
 
+const startButton = document.getElementById('startButton');
+const resetButton = document.getElementById('resetButton');
+
+startButton.addEventListener('click', () => {
+    draw();
+    // Disable the start button
+    startButton.disabled = true;
+});
+resetButton.addEventListener('click', () => {
+    // Clear the balls array
+    balls.length = 0;
+
+    // Repopulate the balls array
+    for (let i = 0; i < 10; i++) {
+        const radius = 10;
+        const x = Math.random() * (canvas.width - 2 * radius) + radius; //position of the ball ensured that the ball is within the canvas
+        const y = Math.random() * (canvas.height - 2 * radius) + radius; //2* radius = diameter substract so that the ball is at least a radius away from the edge of the canvas and than add radius so that the ball is not outside the canvas
+        const speedX = (Math.random() - 0.5) * 4; //first it gives number between 0 and 1, than it substracts 0.5 so that the number is between -0.5 and 0.5 and than it multiplies by 4 so that the number is between -2 and 2 
+        const speedY = (Math.random() - 0.5) * 4; //it ensures that the ball is moving in a random direction
+         //so it's right or down or left or up
+        const ball = new Ball(x, y, radius, speedX, speedY);
+        balls.push(ball);
+    }
+});
+
 class Ball {
     constructor(x, y, radius, speedX, speedY) {
         this.x = x;
@@ -36,11 +61,11 @@ class Ball {
 
 const balls = [];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) { //ale lagi przy 500 XDDDD
     const radius = 10;
-    const x = Math.random() * (canvas.width - 2 * radius) + radius;
+    const x = Math.random() * (canvas.width - 2 * radius) + radius; // this line generates a random number between 0 and canvas.width - 2 * radius
     const y = Math.random() * (canvas.height - 2 * radius) + radius;
-    const speedX = (Math.random() - 0.5) * 4; // Random speed from -2 to 2
+    const speedX = (Math.random() - 0.5) * 4; 
     const speedY = (Math.random() - 0.5) * 4;
 
     const ball = new Ball(x, y, radius, speedX, speedY);
@@ -60,9 +85,11 @@ function draw() {
     // Check the distance between each pair of balls
     for (let i = 0; i < balls.length; i++) {
         for (let j = i + 1; j < balls.length; j++) {
-            const dx = balls[i].x - balls[j].x;
-            const dy = balls[i].y - balls[j].y;
+            const dx = balls[i].x - balls[j].x; //point x - point x = distance between x = length of the side of the triangle dx
+            const dy = balls[i].y - balls[j].y; //point y - point y = distance between y = length of the side of the triangle dy
             const distance = Math.sqrt(dx * dx + dy * dy); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
+            //think of distance as a triangle dx and dy are the sides of the triangle and distance is the hypotenuse(a^2 + b^2 = c^2, so c = sqrt(a^2 + b^2)
+            //https://www.youtube.com/watch?v=xbNXwRWjzSs&ab_channel=MooMooMathandScience
 
             // If the distance is less than 100, draw a line between the balls
             if (distance < 100) {
@@ -78,8 +105,3 @@ function draw() {
     requestAnimationFrame(draw); // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
 }
 
-draw();
-
-//TODO: 
-//1. Add start and reset buttons
-//2. Pythagorean theorem - https://en.wikipedia.org/wiki/Pythagorean_theorem
